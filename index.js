739d5b16b4c0e2d8366eaf3e858a7b20e3cdc628
Krishna-69 <krishnarastogi67@gmail.com>
2025-10-10 21:38:02 +0530

    nodemon

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require("dotenv").config();
+require('@dotenvx/dotenvx').config()
 const { userRouter } = require("./routes/user")
 const { courseRouter } = require("./routes/course")
 const { adminRouter } = require("./routes/admin")
