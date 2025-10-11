d24750acc3243272bd153f91fa50b8c01bbdd55b
Krishna-69 <krishnarastogi67@gmail.com>
2025-10-11 11:26:51 +0530

    Signup/Signin Done

--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,48 @@
 const { Router } = require("express");
 const adminRouter = Router();
-const { adminModel } = require("../db")
+const { adminModel } = require("../db");
+const jwt = require("jsonwebtoken");
+const JWT_ADMIN_PASSWORD = "krishna321321";
 
-adminRouter.post("/signup", function(req, res) {
+adminRouter.post("/signup", async function(req, res) {
+  const { email, password, firstName, lastName } = req.body;
+  //TODO: adding zod validation
+  //TODO: hash the password so plaintext pw is not stored in the DB
+
+  //TODO: Put inside a try catch block
+  await adminModel.create({
+    email: email,
+    password: password,
+    firstName: firstName,
+    lastName: lastName
+  })
+
   res.json({
     message: "singup endpoint"
   })
 })
 
-adminRouter.post("/signin", function(req, res) {
-  res.json({
-    message: "signin endpoint"
+adminRouter.post("/signin", async function(req, res) {
+  const { email, password } = req.body;
+
+  const admin = await adminModel.findOne({
+    email: email,
+    password: password
   })
+
+  if (admin) {
+    const token = jwt.sign({
+      id: admin._id
+    }, JWT_ADMIN_PASSWORD);
+
+    res.json({
+      token: token
+    })
+  } else {
+    res.status(403).json({
+      message: "Incorrect credentials"
+    })
+  }
 })
 
 adminRouter.post("/course", function(req, res) {
